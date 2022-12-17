b8aeb74dcf6b368dfa5f53734cfcfd18924f8ff4
jendrabn <jendra455@gmail.com>
2022-12-18 04:25:48 +0700

    Refactor api routes

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::middleware('throttle:login')->post('/v1/login', [AuthController::class, 'login'])->name('api.v1.login');
-Route::post('/v1/register', [AuthController::class, 'register'])->name('api.v1.register');
-Route::middleware('auth:sanctum')->post('/v1/logout', [AuthController::class, 'logout'])->name('api.v1.logout');
+// Route::middleware('throttle:login')->post('/v1/login', [AuthController::class, 'login'])->name('api.v1.login');
+// Route::post('/v1/register', [AuthController::class, 'register'])->name('api.v1.register');
+// Route::middleware('auth:sanctum')->post('/v1/logout', [AuthController::class, 'logout'])->name('api.v1.logout');
+
+
+
+Route::group(['prefix' => 'v1', 'as' => 'api.v1.'], function () {
+    Route::controller(AuthController::class)->group(
+        function () {
+            Route::post('/login', 'login')->middleware('throttle:login')->name('login');
+            Route::post('/register', 'register')->name('register');
+            Route::post('/logout', 'logout')->middleware('auth:sanctum')->name('logout');
+        }
+    );
+});
